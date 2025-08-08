02455267349a41ccd3c45774b29803d0ebc0a07c
Jack Bennett <jackbennett0708@gmail.com>
2025-08-08 21:24:07 +0100

    Add GPIO for onboard speaker to current test .c src

--- firmware/pwm_test.c
+++ firmware/pwm_test.c
@@ -9,9 +9,12 @@
 #include <hardware/pwm.h>
 #include <pico/stdlib.h>
 
+// GPIO pin for the onboard speaker
+#define GPIO_OBSPKR 22
+
 // GPIO pins for left and right PWM audio, respectively
-#define PWM_AUL 8
-#define PWM_AUR 9
+#define GPIO_PWM_AUL 8
+#define GPIO_PWM_AUR 9
 
 // see 12.5.2: https://datasheets.raspberrypi.com/rp2350/rp2350-datasheet.pdf
 #define SLICE_AUX 4 // GPIO8 and GPIO9
@@ -21,13 +24,22 @@
 int
 main(void)
 {
-    // turn on onboard LED
+    // GPIO init
     gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_init(GPIO_OBSPKR);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_set_dir(GPIO_OBSPKR, GPIO_OUT);
+
+    // turn on onboard LED
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
-    gpio_set_function(PWM_AUL, GPIO_FUNC_PWM);
-    gpio_set_function(PWM_AUR, GPIO_FUNC_PWM);
+    // TODO optionally beep onboard speaker on startup
+    // gpio_put(GPIO_OBSPKR, 1);
+    // sleep_ms(100);
+    // gpio_put(GPIO_OBSPKR, 0);
+
+    gpio_set_function(GPIO_PWM_AUL, GPIO_FUNC_PWM);
+    gpio_set_function(GPIO_PWM_AUR, GPIO_FUNC_PWM);
 
     // e.g. for outputting a C note
     // ref https://datasheets.raspberrypi.com/rp2350/rp2350-datasheet.pdf
