a7079b86657e0cb6dd85fa15efc9e73674f7bd6c
Jack Bennett <jackbennett0708@gmail.com>
2025-08-25 17:26:42 +0100

    Change include directory for kernel symbols

--- firmware/game/init.c
+++ firmware/game/init.c
@@ -5,12 +5,24 @@
  * See the LICENCE file for more information.
  */
 
-#include "panic.h"
+#include "kernel/drivers/vga/stress.h"
+#include "kernel/drivers/vga/text/puts.h"
+#include "kernel/panic.h"
 
 void
 game_main(void)
 {
-    kpanic();
+    vga_stress_palette();
+    vga_stress_grid(20, 10, 0b00011000);
+
+    vga_padvance(7);
+    vga_puts("      *** DOOMbox puts() test ***\n\n\0", 0x0);
+    vga_puts("          (very cool)\0", 0x0);
+    vga_padvance(4);
+    vga_puts("Now in 320x200 resolution!!\n\0", 0x0);
+    vga_puts("(and with double buffering)\0", 0x0);
+
+    vga_stress_atlas(0xFF);
 
     // game main loop
     for (;;) {
--- firmware/kernel/main.c
+++ firmware/kernel/main.c
@@ -17,18 +17,6 @@ main(void)
     kstdio_init(); // no-op in release config
     hal_init();
 
-    vga_stress_palette();
-    vga_stress_grid(20, 10, 0b00011000);
-
-    vga_padvance(7);
-    vga_puts("      *** DOOMbox puts() test ***\n\n\0", 0x0);
-    vga_puts("          (very cool)\0", 0x0);
-    vga_padvance(4);
-    vga_puts("Now in 320x200 resolution!!\n\0", 0x0);
-    vga_puts("(and with double buffering)\0", 0x0);
-
-    vga_stress_atlas(0xFF);
-
     game_main();
 
     return 0;
